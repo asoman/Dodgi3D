6cd04ef8c2bd41be24eb7068411326ba47556af8
NickAk <asoman97@gmail.com>
2018-02-03 22:42:39 +0300

    NOT RDY! moving player

---
+++ Dodgi3D/Assets/Scripts/Player/ModelPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelPlayer : MonoBehaviour {
+
+    public GameObject model;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = model.GetComponent<Animator>();
+    }
+
+    public void StartMoving()
+    {
+        anim.SetBool("IsMoving", true);
+    }
+
+    public void StopMoving()
+    {
+        anim.SetBool("IsMoving", false);
+    }
+
+    public void Death()
+    {
+        anim.SetTrigger("Death");
+    }
+}
--- Dodgi3D/Assets/Scripts/Player/MovePlayer.cs
+++ Dodgi3D/Assets/Scripts/Player/MovePlayer.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour {
 
+    public float speed = 10;
+
+    private ModelPlayer model;
+    private Vector2 CurentDestination;
+    private Coroutine moving;
 
     private void OnEnable()
     {
         InputHandler.OnFingerSwipe += Move;
+        model = GetComponent<ModelPlayer>();
     }
 
     private void OnDisable()
@@ -17,6 +23,25 @@ public class MovePlayer : MonoBehaviour {
 
     public void Move(Vector2 bar)
     {
+        StopCoroutine(moving);
+        CurentDestination += bar;
+        moving = StartCoroutine(MovingCoroutine(CurentDestination));
         transform.Translate(new Vector3(bar.x,0,bar.y));
+    }
+
+    IEnumerator MovingCoroutine(Vector2 destination)
+    {
+        Vector3 destination3D = new Vector3(destination.x, 0, destination.y);
+        float dist = Vector3.Distance(transform.position, destination3D);
+        model.StartMoving();
+        Vector3 moveVector = Vector3.ClampMagnitude(destination3D - transform.position,
+            dist>speed ? speed: dist);
+
+        transform.Translate(moveVector);
+        yield return new WaitForEndOfFrame();
+        if (Vector3.Distance(destination3D, transform.position) <= 0.01)
+            model.StopMoving();
+        else
+            moving = StartCoroutine(MovingCoroutine(destination));
     }
 }
